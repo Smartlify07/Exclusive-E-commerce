744419b053f2ce383047b643a7acc005bf983f37
Obinna Anosike <smartlify09@gmail.com>
2024-07-18 18:45:52 +0100

    Updated link paths in the navbar

--- src/components/ActionIcons.jsx
+++ src/components/ActionIcons.jsx
@@ -4,10 +4,10 @@ import { Link } from "react-router-dom";
 const ActionIcons = ({ children }) => {
   return (
     <div className="flex  gap-5 order-1  lg:flex-row lg:order-2 lg:items-center lg:gap-4">
-      <Link to={"/"} className="">
-        <img src="/images/icons/hearticon.svg" alt="carticon" />
+      <Link to={"/wishlist"} className="">
+        <img src="/images/icons/hearticon.svg" alt="hearticon" />
       </Link>
-      <Link to={"/"} className="">
+      <Link to={"/cart"} className="">
         <img src="/images/icons/carticon.svg" alt="carticon" />
       </Link>
 
