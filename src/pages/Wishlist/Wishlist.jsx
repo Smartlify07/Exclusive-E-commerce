b4db02f4aaf528e019a137aa6aed44a6cc3ee002
Obinna Anosike <smartlify09@gmail.com>
2024-07-20 22:54:18 +0100

    Added loader for pending state

--- src/pages/Wishlist/Wishlist.jsx
+++ src/pages/Wishlist/Wishlist.jsx
@@ -4,16 +4,30 @@ import WishListTopNav from "./WishListTopNav";
 import withProductList from "../../HOCs/withProductList";
 import useWishListProducts from "../../hooks/useWishListProducts";
 import WishListProducts from "./WishListProducts";
-import { selectAllWishListProducts } from "../../app/user/userWishlistSlice";
+import {
+  selectAllWishListProducts,
+  wishListStatus,
+} from "../../app/user/userWishlistSlice";
+import Loader from "../../components/Loader";
+
 const Wishlist = () => {
   const products = useSelector(selectAllWishListProducts);
+  const status = useSelector(wishListStatus);
+
   const ProductList = withProductList(WishListProducts);
   useWishListProducts();
   console.log(products);
+  // const status = useSelector(selectWishListStatus);
   return (
     <main className="py-5 font-poppins flex flex-col items-center justify-center  lg:px-0 lg:py-10">
       <div className="w-10/12">
         <WishListTopNav />
+        <div className="flex items-center py-5 justify-center w-full">
+          {status === "pending" && (
+            <Loader message={"Getting wishlist items"} />
+          )}
+        </div>
+
         <ProductList showDeleteButton products={products} />
         <JustForYou />
       </div>
